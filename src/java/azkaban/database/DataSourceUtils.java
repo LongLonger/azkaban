c6306d58404174e5643e498a2ad321f7ebb6c970
zhongshu <zhongshuhuang@sohu-inc.com>
2017-12-22 11:11:59 +0800

    为了不报错，实现getParentLogger()方法，是一个空实现

--- src/java/azkaban/database/DataSourceUtils.java
+++ src/java/azkaban/database/DataSourceUtils.java
@@ -23,6 +23,8 @@ import org.apache.commons.dbutils.QueryRunner;
 import org.apache.log4j.Logger;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
+import java.sql.SQLFeatureNotSupportedException;
+
 import azkaban.utils.Props;
 
 public class DataSourceUtils {
@@ -150,7 +152,12 @@ public class DataSourceUtils {
 		public String getDBType() {
 			return "mysql";
 		}
-		
+
+		@Override
+		public java.util.logging.Logger getParentLogger() throws SQLFeatureNotSupportedException {
+			return null;
+		}
+
 		private class MonitorThread extends Thread {
 			private static final long MONITOR_THREAD_WAIT_INTERVAL_MS = 30*1000;
 			private boolean shutdown = false;
@@ -218,6 +225,11 @@ public class DataSourceUtils {
 		@Override
 		public String getDBType() {
 			return "h2";
+		}
+
+		@Override
+		public java.util.logging.Logger getParentLogger() throws SQLFeatureNotSupportedException {
+			return null;
 		}
 	}
 	
